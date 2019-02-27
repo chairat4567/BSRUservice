8197eb88ed22f766befd6f108471f0209f56f37f
Chairat <chairat4567@gmail.com>
2019-02-27 14:32:32 +0700

    workshop 27Feb

--- app/src/main/java/th/ac/bsru/chairat/bsruservice/MainFragment.java
+++ app/src/main/java/th/ac/bsru/chairat/bsruservice/MainFragment.java
@@ -28,7 +28,12 @@ public class MainFragment extends Fragment {
         //  Register Controller
         registerController();
 
-
+        //5page
+        page1();
+        page2();
+        page3();
+        page4();
+        page5();
     } //main method
 
     private void registerController() {
@@ -36,9 +41,7 @@ public class MainFragment extends Fragment {
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
        //    Replace Fragment
-
             getActivity()
                     .getSupportFragmentManager()
                     .beginTransaction()
@@ -48,6 +51,86 @@ public class MainFragment extends Fragment {
             }
         });
     }
+
+
+    //  workshop
+
+    private void page1() {
+        TextView textView = getView().findViewById(R.id.page1);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //    Replace Fragment
+                getActivity()
+                        .getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.layoutMainFragmant, new oneFragment())
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+    }
+    private void page2() {
+        TextView textView = getView().findViewById(R.id.page2);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //    Replace Fragment
+                getActivity()
+                        .getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.layoutMainFragmant, new twoFragment())
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+    }
+    private void page3() {
+        TextView textView = getView().findViewById(R.id.page3);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //    Replace Fragment
+                getActivity()
+                        .getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.layoutMainFragmant, new threeFragment())
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+    }
+    private void page4() {
+        TextView textView = getView().findViewById(R.id.page4);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //    Replace Fragment
+                getActivity()
+                        .getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.layoutMainFragmant, new fourFragment())
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+    }
+    private void page5() {
+        TextView textView = getView().findViewById(R.id.page5);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //    Replace Fragment
+                getActivity()
+                        .getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.layoutMainFragmant, new fiveFragment())
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
